e27d93008ef2a71ebd0191feac20517fd6f69f70
root <root@fr-pt-744.jems.local>
2024-09-24 14:26:27 +0200

    add delete invoidces

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -49,3 +49,14 @@ export async function updateInvoice(id: string, formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
+
+  export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+    try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+      revalidatePath('/dashboard/invoices');
+      return { message: 'Deleted Invoice' };
+    } catch (error) {
+      return { message: 'Database Error: Failed to Delete Invoice' };
+    }
+  }
